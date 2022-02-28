c98d723fd7b36bd94420fc506357d199cb74f858
Keloran <keloran@chewedfeed.com>
2022-02-28 17:30:10 +0000

    remove the fontawesome stuff

--- src/main.js
+++ src/main.js
@@ -6,34 +6,7 @@ import store from './store'
 
 import './assets/tailwind.css'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import {
-    faThumbsDown,
-    faThumbsUp,
-    faCheck,
-    faAngleDoubleUp,
-    faAngleDoubleDown,
-    faExchangeAlt,
-} from '@fortawesome/free-solid-svg-icons'
-import {
-    faPlusSquare,
-    faMinusSquare,
-} from '@fortawesome/free-regular-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-library.add(
-    faThumbsDown,
-    faThumbsUp,
-    faCheck,
-    faPlusSquare,
-    faMinusSquare,
-    faAngleDoubleUp,
-    faAngleDoubleDown,
-    faExchangeAlt,
-)
-
 createApp(App)
     .use(router)
     .use(store)
-    .component('font-awesome-icon', FontAwesomeIcon)
     .mount('#app')
